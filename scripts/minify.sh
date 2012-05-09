276a30d960aed46712a2d4bcf00e047f07f6e5eb
Skippy <brov0010@gmail.com>
2012-05-09 11:11:20 -0500

    Turning this into a jQuery plugin and adding a shell script for the minification

---
+++ scripts/minify.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar ~/closure/compiler.jar --js ../src/toaster.js --js_output_file ../toaster.min.js
--- src/toaster.js
+++ src/toaster.js
@@ -1,33 +1,60 @@
 //= require jquery
+(function ($) {
 
-var Toaster = function (query) {
+	/**
+	 *	config:
+	 *		wait: int //specifies the time to wait
+	 *
+	 */
+	var Toaster = function (config) {
+		"use strict";
 
-	var el = $(query),
-		classes = ['error','success'],
-		show = function (msg, callback) {
-			cb = callback || function () { };
-			el.removeClass(classes[0] + ' ' + classes[1]);
-			el[0].innerHTML = '<p>' + msg + '</p>';
-			el.slideDown('slow',function () {
-				el.slideUp('slow', function () {
-					cb();
+		var el = this,
+			classes = ['error','success','warn'],
+			classStr,
+			timer,
+			config = config||{},
+			show = function (msg, callback) {
+				if (el.is(':visible')) {
+					clearTimeout(timer);
+					el.hide();
+				}
+				el.removeClass(classStr);
+				el[0].innerHTML = '<p>' + msg + '</p>';
+				el.slideDown('slow',function () {
+					timer = setTimeout( function () {
+						el.slideUp('slow', callback||function () {});
+					}, config.wait||3000);
 				});
-			}).delay(3000);
-		},
-		error = function (msg, callback) {
-			show(msg, callback);
-			el.addClass(classes[0]);
-		},
-		success = function (msg, callback) {
-			show(msg, callback);
-			el.addClass(classes[1]);
-		},
-		me = this;
+			},
+			error = function (msg, callback) {
+				show(msg, callback);
+				el.addClass(classes[0]);
+			},
+			success = function (msg, callback) {
+				show(msg, callback);
+				el.addClass(classes[1]);
+			},
+			warn = function (msg, callback) {
+				show(msg, callback);
+				el.addClass(classes[2]);
+			},
+			me = this,
+			retval = null;
 
-	me.exists = (el.length>0);
-	if(el.length>0) {
-		me.success = success;
-		me.error = error;
-	}
-	return (el.length>0) ? me : null;
-};
+		me.exists = (el.length>0);
+		if(el.length>0) {
+			$.each(classes, function () {
+				classStr += this + ' ';
+			});
+			me.success = success;
+			me.error = error;
+			me.warn = warn;
+		}
+		retval = (el.length>0) ? me : null;
+		$.toaster = retval;
+		return retval;
+	};
+	
+	$.fn.toaster = Toaster;
+})(jQuery);
